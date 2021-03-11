0fbcd33c97e32e0902a0b3bdc23a2f40a68fb102
tristanhannibal <tristanhannibal@gmail.com>
2021-03-11 16:28:03 -0600

    copy template user input from tests

--- ENSE375/RiskMeter/src/test/java/com/uregina/app/AppTest.java
+++ ENSE375/RiskMeter/src/test/java/com/uregina/app/AppTest.java
@@ -1,20 +1,69 @@
 package com.uregina.app;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
+
+
+import org.junit.*;
+import java.io.*;
 
 /**
  * Unit test for App.
  */
 public class AppTest 
 {
+
+    private final InputStream systemIn = System.in;
+    private final PrintStream systemOut = System.out;
+
+    private ByteArrayInputStream testIn;
+    private ByteArrayOutputStream testOut;
+
+    @Before
+    public void setUpOutput() {
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+    }
+
+    private void provideInput(String data) {
+        testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    private String getOutput() {
+        return testOut.toString();
+    }
+
+    @After
+    public void restoreSystemInputOutput() {
+        System.setIn(systemIn);
+        System.setOut(systemOut);
+    }
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void getAChoice_NonValid_Equals()
     {
-        assertTrue( true );
+    
+	provideInput("5");
+	try{
+        	assertEquals(0, App.getAChoice());
+        }
+        catch(Exception e)
+        {
+        	assertTrue(false);
+        }
     }
-}
+
+
+    @Test
+    public void addAptient()
+    {
+        App app =  new App();
+        PostalCode pc = new PostalCode("K1T-5X2")
+
+        app.addPatient("STeve", "123456780", patientpostalCode, patientAge)
+    }
+ }
