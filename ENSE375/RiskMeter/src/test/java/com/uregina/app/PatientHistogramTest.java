8a1698a4dc1e6558d39cef0fdb2180768c799143
tristanhannibal <tristanhannibal@gmail.com>
2021-03-10 22:24:23 -0600

    add test for histogram

--- ENSE375/RiskMeter/src/test/java/com/uregina/app/PatientHistogramTest.java
+++ ENSE375/RiskMeter/src/test/java/com/uregina/app/PatientHistogramTest.java
@@ -13,8 +13,51 @@ public class PatientHistogramTest
 
 
     @Test
-    void addPatientToARegion_Valid_True(){
+    public void addPatientToARegion_Valid_True(){
         PatientHistogram histo = new PatientHistogram();
+      assertTrue(histo.addAPatientToRegion(10, 5));
+    }
+
+    @Test
+    public void addPatientToARegion_InvalidVertical_False(){
+        PatientHistogram histo = new PatientHistogram();
+        assertFalse(histo.addAPatientToRegion(25, 5));
+
+    }
+
+    @Test
+    public void addPatientToARegion_InvalidHorizontal_False(){
+        PatientHistogram histo = new PatientHistogram();
+        assertFalse(histo.addAPatientToRegion(10, 15));
+
+    }
+    
+
+    
+    @Test
+    public void deleteAPatient_Valid_True(){
+        PatientHistogram histo = new PatientHistogram();
+        histo.addAPatientToRegion(10, 5);
+        assertTrue(histo.deleteAPatientFromRegion(10, 5));
+
+    }
+
+    @Test
+    public void deleteAPatient_Invalid_False(){
+        PatientHistogram histo = new PatientHistogram();
+        assertFalse(histo.deleteAPatientFromRegion(10, 5));
+
+    }
+
+
+    @Test
+    public void getPatientsCountInRegion_True(){
+        PatientHistogram histo = new PatientHistogram();
+        for(int i = 0; i < 5; i++)
+            histo.addAPatientToRegion(10, 5);
+        
+        assertEquals(5,histo.getPatientsCountInRegion(10, 5));
+
     }
 
 }
