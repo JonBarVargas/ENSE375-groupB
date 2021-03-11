7feb9cb5e81dbdcfe1c927986e3fb63614c48baa
tristanhannibal <tristanhannibal@gmail.com>
2021-03-10 22:27:44 -0600

    update tests

--- ENSE375/RiskMeter/src/test/java/com/uregina/app/PatientHistogramTest.java
+++ ENSE375/RiskMeter/src/test/java/com/uregina/app/PatientHistogramTest.java
@@ -7,35 +7,30 @@ import org.junit.Test;
 /**
  * Unit test for PatientHistogram.
  */
-public class PatientHistogramTest
-{
-  
-
+public class PatientHistogramTest {
 
     @Test
-    public void addPatientToARegion_Valid_True(){
+    public void addPatientToARegion_Valid_True() {
         PatientHistogram histo = new PatientHistogram();
-      assertTrue(histo.addAPatientToRegion(10, 5));
+        assertTrue(histo.addAPatientToRegion(10, 5));
     }
 
     @Test
-    public void addPatientToARegion_InvalidVertical_False(){
+    public void addPatientToARegion_InvalidVertical_False() {
         PatientHistogram histo = new PatientHistogram();
         assertFalse(histo.addAPatientToRegion(25, 5));
 
     }
 
     @Test
-    public void addPatientToARegion_InvalidHorizontal_False(){
+    public void addPatientToARegion_InvalidHorizontal_False() {
         PatientHistogram histo = new PatientHistogram();
         assertFalse(histo.addAPatientToRegion(10, 15));
 
     }
-    
 
-    
     @Test
-    public void deleteAPatient_Valid_True(){
+    public void deleteAPatient_Valid_True() {
         PatientHistogram histo = new PatientHistogram();
         histo.addAPatientToRegion(10, 5);
         assertTrue(histo.deleteAPatientFromRegion(10, 5));
@@ -43,20 +38,31 @@ public class PatientHistogramTest
     }
 
     @Test
-    public void deleteAPatient_Invalid_False(){
+    public void deleteAPatient_Invalid_False() {
         PatientHistogram histo = new PatientHistogram();
         assertFalse(histo.deleteAPatientFromRegion(10, 5));
 
     }
 
+    @Test
+    public void getPatientsCountInRegion_True() {
+        PatientHistogram histo = new PatientHistogram();
+        for (int i = 0; i < 5; i++)
+            histo.addAPatientToRegion(10, 5);
+        try {
+            assertEquals(5, histo.getPatientsCountInRegion(10, 5));
+        } catch (IndexOutOfBoundsException e) {
+        }
+
+    }
 
     @Test
-    public void getPatientsCountInRegion_True(){
+    public void getPatientsCountInRegion_invalidOutOfIndexRange_ThrowException() {
         PatientHistogram histo = new PatientHistogram();
-        for(int i = 0; i < 5; i++)
-            histo.addAPatientToRegion(10, 5);
-        
-        assertEquals(5,histo.getPatientsCountInRegion(10, 5));
+        try {
+            histo.getPatientsCountInRegion(25, 5);
+        } catch (IndexOutOfBoundsException e) {
+        }
 
     }
 
