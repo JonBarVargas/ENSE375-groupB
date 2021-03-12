e68c4462be23c30ff755af7092c5e35e580b8728
tristanhannibal <tristanhannibal@gmail.com>
2021-03-11 21:48:15 -0600

    Set up @before

--- ENSE375/RiskMeter/src/test/java/com/uregina/app/PatientHistogramTest.java
+++ ENSE375/RiskMeter/src/test/java/com/uregina/app/PatientHistogramTest.java
@@ -2,36 +2,39 @@ package com.uregina.app;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Unit test for PatientHistogram.
  */
 public class PatientHistogramTest {
+    private PatientHistogram histo;
+
+    @Before
+    public void setUp(){
+         histo = new PatientHistogram();
+    }
 
     @Test
     public void addPatientToARegion_Valid_True() {
-        PatientHistogram histo = new PatientHistogram();
         assertTrue(histo.addAPatientToRegion(10, 5));
     }
 
     @Test
     public void addPatientToARegion_InvalidVertical_False() {
-        PatientHistogram histo = new PatientHistogram();
         assertFalse(histo.addAPatientToRegion(25, 5));
 
     }
 
     @Test
     public void addPatientToARegion_InvalidHorizontal_False() {
-        PatientHistogram histo = new PatientHistogram();
         assertFalse(histo.addAPatientToRegion(10, 15));
 
     }
 
     @Test
     public void deleteAPatient_Valid_True() {
-        PatientHistogram histo = new PatientHistogram();
         histo.addAPatientToRegion(10, 5);
         assertTrue(histo.deleteAPatientFromRegion(10, 5));
 
@@ -39,14 +42,12 @@ public class PatientHistogramTest {
 
     @Test
     public void deleteAPatient_Invalid_False() {
-        PatientHistogram histo = new PatientHistogram();
         assertFalse(histo.deleteAPatientFromRegion(10, 5));
 
     }
 
     @Test
     public void getPatientsCountInRegion_True() {
-        PatientHistogram histo = new PatientHistogram();
         for (int i = 0; i < 5; i++)
             histo.addAPatientToRegion(10, 5);
         try {
@@ -58,7 +59,6 @@ public class PatientHistogramTest {
 
     @Test
     public void getPatientsCountInRegion_invalidOutOfIndexRange_ThrowException() {
-        PatientHistogram histo = new PatientHistogram();
         try {
             histo.getPatientsCountInRegion(25, 5);
         } catch (IndexOutOfBoundsException e) {
