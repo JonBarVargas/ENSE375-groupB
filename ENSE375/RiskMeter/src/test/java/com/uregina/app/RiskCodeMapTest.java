153ef6443139f67c6ec8eddb678e81b8fbdadc37
Jonathan Vargas <jonathanbvargas@hotmail.com>
2021-03-13 00:30:54 -0600

    Add first tests RiskCodeMap
    Added return_Risk_In_A_Region and update_Risks_Ideal_input

--- ENSE375/RiskMeter/src/test/java/com/uregina/app/RiskCodeMapTest.java
+++ ENSE375/RiskMeter/src/test/java/com/uregina/app/RiskCodeMapTest.java
@@ -4,6 +4,10 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import jdk.jfr.Timestamp;
+
+import java.util.*;
+
 /**
  * Unit test for RiskCodeMap.
  */
@@ -16,5 +20,29 @@ public class RiskCodeMapTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void return_Risk_In_A_Region()
+    {   
+        boolean correctCode = false;
+        RiskCodeMap risk = new RiskCodeMap();
+        if(risk.getRiskInARegion(0, 2) == 'G')
+            correctCode = true;
+        assertTrue(correctCode);
+    }
+
+    @Test
+    public void update_Risks_Ideal_input()
+    {
+        int VIndex = 0; 
+        int HIndex = 2;
+        RiskCodeMap risk = new RiskCodeMap();
+        ArrayList cases = new ArrayList();
+        cases.add(2);
+        cases.add(3);
+        cases.add(4);
+       
+        assertTrue( risk.updateRiskInARegion(VIndex, HIndex, 3, cases));
     }
 }
