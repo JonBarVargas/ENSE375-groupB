0a3e461bc6ed46aea7e626b10324ac6fe4b8c094
karleefidek <55856535+karleefidek@users.noreply.github.com>
2021-03-13 19:28:06 -0600

    Update PatientList.java

--- ENSE375/RiskMeter/src/main/java/com/uregina/app/PatientList.java
+++ ENSE375/RiskMeter/src/main/java/com/uregina/app/PatientList.java
@@ -1,56 +1,57 @@
+//Karlee Fidek
 package com.uregina.app;
-// import com.uregina.exception.*;
 import java.util.ArrayList;
 
-public class PatientList 
-{
+public class PatientList {
 	private ArrayList<Patient> patientList;
-    	public PatientList() 
-    	{
-    		patientList = new ArrayList<Patient>();
+	
+    	public PatientList() {
+    	    patientList = new ArrayList<Patient>();
     	}
+	
     	/**
 	*
 	* @return boolean: true if the patient can be added to the list 
 	*/
-    	public boolean addPatient(Patient patient)
-    	{
-			if(patient == null) return false;
-    		try
-			{
-				patientList.add(patient);
-			}catch(Exception e)
-			{
-				return false;
-			}
-    		return true;
+    	public boolean addPatient(Patient patient) {
+	    if(patient == null) {
+	        return false;
+	    }
+    	
+	    try {
+	        patientList.add(patient);
+	    } catch(Exception e) {
+	        return false;
+	    }
+    	
+	    return true;
     	}
+	
     	/**
 	*
 	* @return boolean: true if the patient is removed from the list 
 	*/
-    	public boolean deletePatient(int index)
-    	{
-			try
-			{
-				patientList.remove(index);
-			}catch(IndexOutOfBoundsException e)
-			{ 
-				return false;
-			}
-    		return true;
+    	public boolean deletePatient(int index) {
+	    try {
+		patientList.remove(index);
+	    } catch(IndexOutOfBoundsException e) { 
+		return false;
+	    }
+		
+    	    return true;
     	}
-    	public Patient getPatient(String ID)
-    	{
-			for (Patient patient : patientList) {
-				if (patient.getID().equals(ID)) {
-					return patient;
-				}
-			}
-			return null;
+	
+    	public Patient getPatient(String ID) {
+	    for (Patient patient : patientList) {
+		if (patient.getID().equals(ID)) {
+		    return patient;
+		}
+    	    }
+		
+	    return null;
     	}
-    	public int getNumberofPatients()
-    	{
-    		return patientList.size();
+	
+    	public int getNumberofPatients() {
+    	    return patientList.size();
     	}
 }
