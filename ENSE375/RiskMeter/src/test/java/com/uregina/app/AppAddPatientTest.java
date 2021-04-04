5ad080286b0d8fa37d9110d6547863c7829b4bbe
Karlee Fidek <kdf131@uregina.ca>
2021-04-03 22:25:00 -0600

    Edit test cases

--- ENSE375/RiskMeter/src/test/java/com/uregina/app/AppAddPatientTest.java
+++ ENSE375/RiskMeter/src/test/java/com/uregina/app/AppAddPatientTest.java
@@ -13,37 +13,37 @@ public class AppAddPatientTest {
 	@Test
 	public void addPatient_validPatient_true() {
 		boolean addSuccessful = app.addPatient("Karlee", "112233445", "A4A-2F7", 21);
-		assertTrue(false);
+		assertTrue(addSuccessful);
 	}
 	
 	@Test
 	public void addPatient_invalidPostalCode_false() {
 		boolean addSuccessful = app.addPatient("Karlee", "112233445", "A4G3H8", 21);
-		assertFalse(true);
+		assertFalse(addSuccessful);
 	}
 	
 	@Test
 	public void addPatient_invalidAge_false() {
 		boolean addSuccessful = app.addPatient("Karlee", "112233445", "A4A-2F7", -1);
-		assertFalse(true);
+		assertFalse(addSuccessful);
 	}
 	
 	@Test
 	public void addPatient_invalidId_false() {
 		boolean addSuccessful = app.addPatient("Karlee", "1234", "A4A-2F7", 21);
-		assertFalse(true);
+		assertFalse(addSuccessful);
 	}
 	
 	@Test
 	public void addPatient_invalidName_false() {
 		boolean addSuccessful = app.addPatient("Karlee&Fidek", "112233445", "A4A-2F7", 21);
-		assertFalse(true);
+		assertFalse(addSuccessful);
 	}
 	
 	@Test
 	public void addPatient_RiskMap_false() {
 		app.addPatient("Karlee", "112233445", "A4A-2F7", 21);
 		boolean addSuccessful = app.addPatient("Dr. ElDarieby", "123456789", "A4A-2F7", 40);
-		assertFalse(true);
+		assertFalse(addSuccessful);
 	}
 }
