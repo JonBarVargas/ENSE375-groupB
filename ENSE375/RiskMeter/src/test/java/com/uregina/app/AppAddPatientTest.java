bd35b362a07b41a59499af2be88bb83cd61b3a5a
Karlee Fidek <kdf131@uregina.ca>
2021-04-04 21:07:00 -0600

    Add test

--- ENSE375/RiskMeter/src/test/java/com/uregina/app/AppAddPatientTest.java
+++ ENSE375/RiskMeter/src/test/java/com/uregina/app/AppAddPatientTest.java
@@ -46,4 +46,10 @@ public class AppAddPatientTest {
 		boolean addSuccessful = app.addPatient("Dr. ElDarieby", "112233445", "K1A-2F7", 40);
 		assertFalse(addSuccessful);
 	}
+
+	@Test
+	public void addPatient_validPatient_true() {
+		boolean addSuccessful = app.addPatient("Karlee", "123456789", "K1A-3T6", 21);
+		assertTrue(addSuccessful);
+	}
 }
