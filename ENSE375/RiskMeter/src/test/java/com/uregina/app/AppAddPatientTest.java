766ebb7e5b001f2361b3d13a7136be14bf47454f
Karlee Fidek <kdf131@uregina.ca>
2021-04-05 21:42:55 -0600

    Add tests

--- ENSE375/RiskMeter/src/test/java/com/uregina/app/AppAddPatientTest.java
+++ ENSE375/RiskMeter/src/test/java/com/uregina/app/AppAddPatientTest.java
@@ -1,8 +1,7 @@
 //Karlee Fidek
 package com.uregina.app;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.io.*;
 import org.junit.*;
@@ -12,17 +11,18 @@ public class AppAddPatientTest {
 	private App app;
 	
 	private final InputStream systemIn = System.in;
-	private final OutputStream systemOut = System.out;
+	private final PrintStream systemOut = System.out;
 
 	private ByteArrayInputStream testIn;
 	private ByteArrayOutputStream testOut;
-
+	private String separator;
 
 	@Before
 	public void setUpOutput() {
 		app = new App();
 		testOut = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(testOut));
+		separator = System.getProperty("line.separator");
 	}
 
 	private void provideInput(String data) {
@@ -45,7 +45,12 @@ public class AppAddPatientTest {
 		boolean addSuccessful = app.addPatient("Karlee", "112233445", "K1A-2F7", 21);
 		assertTrue(addSuccessful);
 	}
-	
+
+	//
+	//Only need one of each failure case because the specific fail cases for each type of failure
+	//(ID, postal code, name, age) are tested in the specific Class test classes
+	//
+
 	@Test
 	public void addPatient_invalidPostalCode_false() {
 		boolean addSuccessful = app.addPatient("Karlee", "123456780", "A4G3H8", 21);
@@ -76,4 +81,68 @@ public class AppAddPatientTest {
 		boolean addSuccessful = app.addPatient("Dr. ElDarieby", "112233445", "K1A-2F7", 40);
 		assertFalse(addSuccessful);
 	}
+
+	@Test
+        public void addPatient_adding2Patients_true() {
+                app.addPatient("Karlee", "112233445", "K1A-2F7", 21);
+                boolean addSuccessful = app.addPatient("Dr. ElDarieby", "123456789", "K1A-2F7", 40);
+                assertTrue(addSuccessful);
+        }
+
+	//--------------------------------- Console Input Tests --------------------------------------//
+	
+
+	@Test
+        public void addPatient_validPatientInput_true() {
+		provideInput("Karlee" + separator + "112233445" + separator + "K1A-2F7" + separator + "21" + separator);
+		App.addPatientOption(app);
+		String expectedOutput = "\tPatient has been added successfully";
+                String actualOutput = getOutput();
+		assertEquals(expectedOutput, actualOutput);
+        }
+
+        //
+        //Only need one of each failure case because the specific fail cases for each type of failure
+        //(ID, postal code, name, age) are tested in the specific Class test classes
+        //
+
+	/*
+        @Test
+        public void addPatient_invalidPostalCode_false() {
+                boolean addSuccessful = app.addPatient("Karlee", "123456780", "A4G3H8", 21);
+                assertFalse(addSuccessful);
+        }
+
+        @Test
+        public void addPatient_invalidAge_false() {
+                boolean addSuccessful = app.addPatient("Karlee", "112233445", "K1A-2F7", -1);
+                assertFalse(addSuccessful);
+        }
+
+        @Test
+        public void addPatient_invalidId_false() {
+                boolean addSuccessful = app.addPatient("Karlee", "1234", "K1A-2F7", 21);
+                assertFalse(addSuccessful);
+        }
+
+        @Test
+        public void addPatient_invalidName_false() {
+                boolean addSuccessful = app.addPatient("Karlee&Fidek", "112233445", "K1A-2F7", 21);
+                assertFalse(addSuccessful);
+        }
+
+        @Test
+        public void addPatient_addingToPatientListFails_false() {
+                app.addPatient("Karlee", "112233445", "K1A-2F7", 21);
+                boolean addSuccessful = app.addPatient("Dr. ElDarieby", "112233445", "K1A-2F7", 40);
+                assertFalse(addSuccessful);
+        }
+
+        @Test
+        public void addPatient_adding2Patients_true() {
+                app.addPatient("Karlee", "112233445", "K1A-2F7", 21);
+                boolean addSuccessful = app.addPatient("Dr. ElDarieby", "123456789", "K1A-2F7", 40);
+                assertTrue(addSuccessful);
+        }
+	*/
 }
