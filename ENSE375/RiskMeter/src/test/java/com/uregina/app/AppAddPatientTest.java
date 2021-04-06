804c604ed0d8bc75dfac321c12565b44be0052cf
karleefidek <55856535+karleefidek@users.noreply.github.com>
2021-04-05 21:55:18 -0600

    Update AppAddPatientTest.java

--- ENSE375/RiskMeter/src/test/java/com/uregina/app/AppAddPatientTest.java
+++ ENSE375/RiskMeter/src/test/java/com/uregina/app/AppAddPatientTest.java
@@ -93,12 +93,12 @@ public class AppAddPatientTest {
 	
 
 	@Test
-        public void addPatient_validPatientInput_true() {
+        public void addPatient_validPatientInput_successful() {
 		provideInput("Karlee" + separator + "112233445" + separator + "K1A-2F7" + separator + "21" + separator);
 		App.addPatientOption(app);
 		String expectedOutput = "\tPatient has been added successfully";
                 String actualOutput = getOutput();
-		assertEquals(expectedOutput, actualOutput);
+		assertTrue(actualOutput.contains(expectedOutput));
         }
 
         //
@@ -106,43 +106,69 @@ public class AppAddPatientTest {
         //(ID, postal code, name, age) are tested in the specific Class test classes
         //
 
-	/*
         @Test
-        public void addPatient_invalidPostalCode_false() {
-                boolean addSuccessful = app.addPatient("Karlee", "123456780", "A4G3H8", 21);
-                assertFalse(addSuccessful);
+        public void addPatient_invalidPostalCodeInput_fails() {
+		provideInput("Karlee" + separator + "123456780" + separator + "A4G3H8" + separator + "21" + separator);
+		App.addPatientOption(app);
+		String expectedOutput = "\tPatient failed to be added";
+                String actualOutput = getOutput();
+		assertTrue(actualOutput.contains(expectedOutput));
         }
 
         @Test
-        public void addPatient_invalidAge_false() {
-                boolean addSuccessful = app.addPatient("Karlee", "112233445", "K1A-2F7", -1);
-                assertFalse(addSuccessful);
+        public void addPatient_invalidAgeInput_fails() {
+		provideInput("Karlee" + separator + "112233445" + separator + "K1A-2F7" + separator + "-1" + separator);
+		App.addPatientOption(app);
+		String expectedOutput = "\tPatient failed to be added";
+                String actualOutput = getOutput();
+		assertTrue(actualOutput.contains(expectedOutput));
         }
 
         @Test
-        public void addPatient_invalidId_false() {
-                boolean addSuccessful = app.addPatient("Karlee", "1234", "K1A-2F7", 21);
-                assertFalse(addSuccessful);
+        public void addPatient_invalidIdInput_fails() {
+		provideInput("Karlee" + separator + "1234" + separator + "K1A-2F7" + separator + "21" + separator);
+		App.addPatientOption(app);
+		String expectedOutput = "\tPatient failed to be added";
+                String actualOutput = getOutput();
+		assertTrue(actualOutput.contains(expectedOutput));
         }
 
         @Test
-        public void addPatient_invalidName_false() {
-                boolean addSuccessful = app.addPatient("Karlee&Fidek", "112233445", "K1A-2F7", 21);
-                assertFalse(addSuccessful);
+        public void addPatient_invalidNameInput_fails() {
+		provideInput("Karlee&Fidek" + separator + "112233445" + separator + "K1A-2F7" + separator + "21" + separator);
+		App.addPatientOption(app);
+		String expectedOutput = "\tPatient failed to be added";
+                String actualOutput = getOutput();
+		assertTrue(actualOutput.contains(expectedOutput));
         }
 
         @Test
-        public void addPatient_addingToPatientListFails_false() {
-                app.addPatient("Karlee", "112233445", "K1A-2F7", 21);
-                boolean addSuccessful = app.addPatient("Dr. ElDarieby", "112233445", "K1A-2F7", 40);
-                assertFalse(addSuccessful);
+        public void addPatient_addingToPatientListWithInputFails_fails() {
+		provideInput("Karlee" + separator + "112233445" + separator + "K1A-2F7" + separator + "21" + separator);
+		App.addPatientOption(app);
+		String expectedOutput = "\tPatient has been added successfully";
+                String actualOutput = getOutput();
+		assertTrue(actualOutput.contains(expectedOutput));
+		
+		provideInput("Dr. ElDarieby" + separator + "112233445" + separator + "K1A-2F7" + separator + "40" + separator);
+		App.addPatientOption(app);
+		String expectedOutput = "\tPatient failed to be added";
+                String actualOutput = getOutput();
+		assertTrue(actualOutput.contains(expectedOutput));
         }
 
         @Test
-        public void addPatient_adding2Patients_true() {
-                app.addPatient("Karlee", "112233445", "K1A-2F7", 21);
-                boolean addSuccessful = app.addPatient("Dr. ElDarieby", "123456789", "K1A-2F7", 40);
-                assertTrue(addSuccessful);
+        public void addPatient_adding2PatientsWithInput_successful() {
+		provideInput("Karlee" + separator + "112233445" + separator + "K1A-2F7" + separator + "21" + separator);
+		App.addPatientOption(app);
+		String expectedOutput = "\tPatient has been added successfully";
+                String actualOutput = getOutput();
+		assertTrue(actualOutput.contains(expectedOutput));
+		
+		provideInput("Dr. ElDarieby" + separator + "123456789" + separator + "K1A-2F7" + separator + "40" + separator);
+		App.addPatientOption(app);
+		String expectedOutput = "\tPatient has been added successfully";
+                String actualOutput = getOutput();
+		assertTrue(actualOutput.contains(expectedOutput));
         }
-	*/
 }
