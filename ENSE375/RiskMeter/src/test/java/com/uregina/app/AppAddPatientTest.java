0031b09bc158bf8a740c98e122c22d505824f2da
Karlee Fidek <kdf131@uregina.ca>
2021-04-05 21:14:17 -0600

    Add input output streams to test

--- ENSE375/RiskMeter/src/test/java/com/uregina/app/AppAddPatientTest.java
+++ ENSE375/RiskMeter/src/test/java/com/uregina/app/AppAddPatientTest.java
@@ -4,12 +4,42 @@ package com.uregina.app;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import java.io.*;
+import org.junit.*;
 
 public class AppAddPatientTest {
 
-	App app = new App();
+	private App app;
 	
+	private final InputStream systemIn = System.in;
+	private final OutputStream systemOut = System.out;
+
+	private ByteArrayInputStream testIn;
+	private ByteArrayOutputStream testOut;
+
+
+	@Before
+	public void setUpOutput() {
+		app = new App();
+		testOut = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(testOut));
+	}
+
+	private void provideInput(String data) {
+	        testIn = new ByteArrayInputStream(data.getBytes());
+        	System.setIn(testIn);
+	}
+
+	private String getOutput() {
+        	return testOut.toString();
+    	}
+
+    	@After
+    	public void restoreSystemInputOutput() {
+        	System.setIn(systemIn);
+        	System.setOut(systemOut);
+    	}
+
 	@Test
 	public void addPatient_validPatient_true() {
 		boolean addSuccessful = app.addPatient("Karlee", "112233445", "K1A-2F7", 21);
